d50e58d31c38aaa0a0d07f613cb7e5845d33aad3
Carlos <carlosalej.perez@gmail.com>
2015-06-07 11:54:55 +0200

    Primera página y favicon

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
+
+router.get('/author', function(req, res){
+  res.render('author', {
+    title: 'Créditos'
+  });
+});
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+//router.get('author', quizController.author);
 
 module.exports = router;

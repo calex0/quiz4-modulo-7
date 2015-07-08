39eef22d98cfafcff18a251f3b4caec779be4b93
Carlos <carlosalej.perez@gmail.com>
2015-07-08 15:10:29 +0200

    modulo 9 no guarda comentarios en SQLite

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,25 @@
 var models = require('../models/models.js');
 
+// Autoload :id de comentarios
+exports.load = function(req, res, next, commentId) {
+  models.Comment.find({
+            where: {
+                id: Number(commentId)
+            }
+        }).then(function(comment) {
+      if (comment) {
+        req.comment = comment;
+        next();
+      } else {
+        next(new Error('No existe commentId=' + commentId));
+      }
+    }
+  ).catch(function(error){next(error)});
+};
+
+
+
+
 // GET /quizes/:quizId/comments/new
 exports.new = function(req, res) {
 	res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
@@ -8,8 +28,8 @@ exports.new = function(req, res) {
 
 // POST /quizes:quizId/comments
 exports.create = function(req, res) {
-	var comment = models.comment.build(
-		{ texto: req.body.comments.texto,
+	var comment = models.Comment.build(
+		{ texto: req.body.comment.texto,
 		  QuizId: req.params.quizId
 		});
 		
@@ -18,7 +38,7 @@ exports.create = function(req, res) {
 				if (err) {
 					res.render('comments/new.ejs', {comment: comment,  quizid: req.params.quizId, errors: err.errors});
 				} else { //save: guarda en BD campo de texto comment
-					comment.save().then( function(error){ res.redirect('/quizes/'+req.params.quizId);});
+					comment.save().then( function(){ res.redirect('/quizes/'+req.params.quizId);});
 				}
 			}
 		).catch(function(error){next(error);});
